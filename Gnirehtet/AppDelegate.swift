fb96d23291c6933bba8532f62c8220dac739d900
Ryan Wirth <ryan@ryanwirth.ca>
2020-04-23 14:28:24 -0600

    Add WiFi icon to menu bar

--- Gnirehtet/AppDelegate.swift
+++ Gnirehtet/AppDelegate.swift
@@ -11,29 +11,24 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    var window: NSWindow!
-
-
+    
+    let statusItem: NSStatusItem = {
+        let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        item.button?.image = NSImage(named: "baseline_wifi_tethering_black_18pt")
+        item.button?.action = #selector(helloWorld(_:))
+        return item
+    }()
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        // TODO
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        // TODO
     }
-
-
+    
+    @objc func helloWorld(_ sender: Any?) {
+        print("Hello World!")
+    }
+    
 }
-
