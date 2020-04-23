a936eaae5a930142f520b1d61ed986aab96c024a
Ryan Wirth <ryan@ryanwirth.ca>
2020-04-23 13:50:36 -0600

    Initial Commit

---
+++ Gnirehtet/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Gnirehtet
+//
+//  Created by Ryan Wirth on 2020-04-23.
+//  Copyright © 2020 Ryan Wirth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
